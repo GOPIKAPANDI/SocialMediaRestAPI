38b623ff23798fce9fdad7ea55e4d2e351748c03
GOPIKAPANDI <gopikapandi.r2021csea@sece.ac.in>
2023-11-01 19:24:48 +0530

    SocialMedia RestAPI Creation Completed

---
+++ Routes/AuthRoute.js
@@ -0,0 +1,12 @@
+import express from "express"; 
+import { loginUser, registerUser } from "../Controllers/AuthController.js"; 
+
+const router = express.Router() 
+
+// after '/auth' if we have '/'(nothing) then it is executed  [For testing Purpose] 
+// router.get('/',async(req,res)=>{res.send("Auth Route")}) 
+
+router.post('/register' , registerUser) 
+router.post('/login', loginUser)   
+
+export default router 
